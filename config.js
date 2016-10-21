adeccbd0f1588febccee53405debc75d46cdba75
Bassem Ghoniem <bassem.ghoniem@yahoo.com>
2016-10-22 00:31:23 +0200

    updating database link to work with mongo container

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
  */
 module.exports = {
   secretKey: '12345-67890-09876-54321', // used for generating JWT tokens.
-  mongodbUrl: 'mongodb://localhost/bookAPI',
+  mongodbUrl: 'mongodb://mongo:27017/bookAPI',
 };
